e37a3d8351fc44facf0c575cdcb1b91defecc2ee
Yashovardhan Siramdas <ced16i028@iiitdm.ac.in>
2018-10-10 01:08:12 +0530

    Modified: require method in server's index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,5 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (app) => {
-  // API routes
-  fs.readdirSync(__dirname + '/api/').forEach((file) => {
-    require(`./api/${file.substr(0, file.indexOf('.'))}`)(app);
-  });
+  require('./api/signup_login.js')(app)
 };
