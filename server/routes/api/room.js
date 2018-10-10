8a682ab441475c424d664687d13871dbfb36c434
Yashovardhan Siramdas <ced16i028@iiitdm.ac.in>
2018-10-10 21:08:01 +0530

    Added: Backend endpoint for joinRoom

--- server/routes/api/room.js
+++ server/routes/api/room.js
@@ -38,7 +38,7 @@ module.exports = (app) => {
 	      });
 	    }
 
-	    // Save the new user
+	    // Save the new room
 	    const newRoom = new Room();
 
 	    newRoom.roomName = roomName;
@@ -53,6 +53,63 @@ module.exports = (app) => {
 	      return res.send({
 	        success: true,
 	        message: 'Successfully created room and joined the user in room',
+	        roomName: doc.roomName,
+	        email: doc.email
+	      });
+	    });
+	  });
+
+	});
+
+	app.post('/api/joinRoom', function (req, res, next) {
+	  let {body} = req;
+	  let {roomName} = body;
+	  let {email} = body;
+
+	  if (!roomName) {
+	    return res.send({
+	      success: false,
+	      message: 'Error: roomName cannot be blank.'
+	    });
+	  }
+	  if (!email) {
+	    return res.send({
+	      success: false,
+	      message: 'Error: Email cannot be blank.'
+	    });
+	  }
+	  email = email.toLowerCase();
+	  email = email.trim();
+
+
+	  Room.find({
+	    roomName: roomName
+	  }, (err, records) => {
+	    if (err) {
+	      return res.send({
+	        success: false,
+	        message: 'Error: Server error'
+	      });
+	    } else if (records.length == 0) {
+	      return res.send({
+	        success: false,
+	        message: 'Error: Room doesnt exist.'
+	      });
+	    }
+	    const newRoom = new Room();
+
+	    newRoom.roomName = roomName;
+	    newRoom.email = email;
+	    newRoom.save((err, doc) => {
+	      if (err) {
+	        return res.send({
+	          success: false,
+	          message: 'Error: Server error'
+	        });
+	      }
+	      return res.send({
+	        success: true,
+	        message: 'Successfully joined the user in room',
 	        roomName: doc.roomName,
 	        email: doc.email
 	      });
