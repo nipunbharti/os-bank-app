10353e61dc45471db203f1b33d6d3cbc57bad657
Yashovardhan Siramdas <ced16i028@iiitdm.ac.in>
2018-11-23 12:09:02 +0530

    Removed config.js from gitignore for heroku deployment

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb://yasho:zerone1234@ds111063.mlab.com:11063/zerone',
+  db_dev: 'mongodb://yasho:zerone1234@ds111063.mlab.com:11063/zerone',
+};
